f94cfef5854387f6936f5089e362b97f66ff0bd1
AndreyAntonov <andru2517@bk.ru>
2022-10-05 15:09:10 +0400

    create weather app project

---
+++ weatherApp/weatherApp/WeatherViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Андрей Антонов on 05.10.2022.
+//  Copyright © 2022 Андрей Антонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
