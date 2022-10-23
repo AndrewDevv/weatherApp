3d9e1ffb9507e9e5911d6d5e739395d599d2820f
AndreyAntonov <andru2517@bk.ru>
2022-10-23 17:45:48 +0400

    Создал таблицу и ячейки, добавил иконки в проект

---
+++ weatherApp/weatherApp/HourlyCollectionCell/HourlyCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  HourlyCollectionViewCell.swift
+//  weatherApp
+//
+//  Created by Андрей Антонов on 23.10.2022.
+//  Copyright © 2022 Андрей Антонов. All rights reserved.
+//
+
+import UIKit
+
+class HourlyCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- weatherApp/weatherApp/WeatherViewController.swift
+++ weatherApp/weatherApp/WeatherViewController.swift
@@ -9,11 +9,71 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+
+        return tableView
+    }()
+    
+    private var weatherModel = [Weather]()
+    
+    // MARK: Lyfecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        
+        setupView()
+        setupTableView()
+        setupConstraint()
+    }
+    
+    // MARK: Setup
+    
+    private func setupTableView() {
+        tableView.register(
+            DailyTableViewCell.self,
+            forCellReuseIdentifier: String(describing: DailyTableViewCell.self)
+        )
+        tableView.register(
+            HourlyCollectionViewCell.self,
+            forCellReuseIdentifier: String(describing: HourlyCollectionViewCell.self)
+        )
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraint() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
     }
 }
 
+// MARK: Delegete and DataSource
+
+extension WeatherViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weatherModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: DailyTableViewCell.self)) as! DailyTableViewCell
+        
+        return cell
+    }
+}
